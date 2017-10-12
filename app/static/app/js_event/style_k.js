adf5881d2be2b4af1ad618ad5567a7fa162591f2
v_solution <v_solution@yopmail.com>
2017-10-12 15:13:50 -0500

    tab actividades 80%

--- app/static/app/js_event/style_k.js
+++ app/static/app/js_event/style_k.js
@@ -12,26 +12,42 @@ $(function () {
         load_maquinarias("#cbo_maq", $(this).val());
     });
     $("#btn_add_activity").click(function(){
-
-
-
         id_actividad = $("#cboTipoActividad").selectpicker("val");
-        console.log(cbo_option("#cboTipoActividad"));
+        datos = cbo_option("#cboTipoActividad");
+        bandera = true;
+        actividades = [];
+        if(!$.isEmptyObject($("#cont-actividades").data("actividades"))){
+            actividades = $("#cont-actividades").data("actividades");
+            if($.inArray(datos.id, actividades) == -1){
+                actividades.push(datos.id);
+                $("#cont-actividades").data("actividades",actividades);
+            }
+            else {
+                alert("Actividad agregada");
+                bandera = false;
+            }
+        }else{
+            actividades.push(datos.id);
+            $("#cont-actividades").data("actividades",actividades);
+        }
+        if(bandera){
+            sub_actividades = getSubActividades(id_actividad);
+            actividad_sample = $("#cont-actividades .actividad_sample").clone();
+            $(actividad_sample).removeClass("actividad_sample");
+            $(actividad_sample).removeClass("hidden");
+            $(actividad_sample).find("span[name='titulo_actividad']").html(datos.descripcion);
+            $(actividad_sample).find("button[name='btn_del_actividad']").data("id",datos.id);
 
-        sub_actividades = getSubActividades(id_actividad);
-        actividad_sample = $("#cont-actividades .actividad_sample").clone();
-        $(actividad_sample).removeClass("actividad_sample");
-        $(actividad_sample).removeClass("hidden");
-
-        $.each(sub_actividades,function(i,sub_actividad){
-            op_subactividad = $(actividad_sample).find(".op_subactividad").clone();
-            $(op_subactividad).removeClass("hidden");
-            $(op_subactividad).removeClass("op_subactividad");
-            $(op_subactividad).find(".buying-selling-word").html(sub_actividad.descripcion);
-            $(actividad_sample).find(".buying-selling-group").append(op_subactividad);
-        });
-
-        $("#cont-actividades").append(actividad_sample);
+            $.each(sub_actividades,function(i,sub_actividad){
+                op_subactividad = $(actividad_sample).find(".op_subactividad").clone();
+                $(op_subactividad).removeClass("hidden");
+                $(op_subactividad).removeClass("op_subactividad");
+                $(op_subactividad).find(".buying-selling-word").html(sub_actividad.descripcion);
+                $(op_subactividad).find("input[type]").attr("type",(datos.op_seleccion === 1)?"radio":"checkbox");
+                $(actividad_sample).find(".buying-selling-group").append(op_subactividad);
+            });
+            $("#cont-actividades").append(actividad_sample);
+        }
     });
 
 
@@ -43,6 +59,16 @@ $(function () {
         $(op_area).addClass("add_area");
         $(this).closest(".row").find(".content").append(op_area);
     });
+
+    $("#exTab2").on("click","button[name='btn_del_actividad']",function () {
+        actividades = $("#cont-actividades").data("actividades");
+        id = $(this).data("id");
+        actividades.splice($.inArray(id, actividades),1);
+        $("#cont-actividades").data("actividades",actividades)
+        $(this).closest(".row").remove();
+    });
+
+
 
     $("#tab_actividades").on("click", ".delete", function () {
         $(this).closest(".row").remove();
